6cfe505f22e1780fd0af35de7cb356811032803f
DuDuViPss <145750086+DuDuViPss@users.noreply.github.com>
2023-10-16 15:39:05 +0500

    Исправление 3 способа

--- spiral2.py
+++ spiral2.py
@@ -1,6 +1,15 @@
 n, m = [int(i) for i in input().split()]
 spiral = [[0] * m for _ in range(n)]
 c = 1
+    for k in range(min(n // 2 + 1, m //2 + 1)):  
+    for j in range(k, m - k):  
+        if spiral[k][j] == 0:  
+            spiral[k][j] = c 
+            c += 1
+    for i in range(1 + k, n - k):  
+        if spiral[i][m - k - 1] == 0:
+            spiral[i][m - k - 1] = c 
+            c += 1
     for j in range(m - k - 2, k - 1, -1):  
         if spiral[n - k - 1][j] == 0:
             spiral[n - k - 1][j] = c 
